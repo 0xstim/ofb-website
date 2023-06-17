20eabcb2241045ff7e142b1d418b96e06bc8ae09
Victor Luong <victor@Victors-MacBook-Pro.local>
2023-06-18 00:12:48 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Open For Builders</h1>
+      <blockquote>Test</blockquote>
+      <p>Open for builders is a </p>
+    </div>
+  )
+}
